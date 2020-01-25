a52feb052e5830180c93ac094ecefbca6c5474ec
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-01-25 23:37:48 +0300

    Add errors reducers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import ACTIONS from './constants';
-import { goal, skiDay } from './store/reducers';
+import { goal, skiDay, error } from './store/reducers';
 
 const setGoalState = 10;
 const skiDayState = null;
+const errorState = ['user not authorized', 'server feed not found'];
 
 const setGoalAction = {
   type: ACTIONS.SET_GOAL,
@@ -17,14 +18,27 @@ const skiDayAction = {
     backcountry: false,
   },
 };
+const addErrorAction = {
+  type: ACTIONS.ADD_ERROR,
+  payload: "can't connect to server",
+};
+const removeErrorAction = {
+  type: ACTIONS.CLEAR_ERROR,
+  payload: 0,
+};
 
 const nextSetGoalState = goal(setGoalState, setGoalAction);
 const nextSkiDayState = skiDay(skiDayState, skiDayAction);
+const newErrorState = error(errorState, addErrorAction);
+const removedErrorState = error(errorState, removeErrorAction);
 
+// eslint-disable-next-line
 console.log(`
   initial goal: ${skiDayState}
   set goal action: ${JSON.stringify(setGoalAction)}
   ski day action: ${JSON.stringify(skiDayAction)}
   new goal: ${nextSetGoalState}
   new day: ${JSON.stringify(nextSkiDayState)}
+  new error: ${JSON.stringify(newErrorState)}
+  removed error: ${JSON.stringify(removedErrorState)}
 `);
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,13 +1,20 @@
 import ACTIONS from '../constants';
 
 const goal = (state = 10, { type, payload }) =>
-  type === ACTIONS.SET_GOAL
-    ? parseInt(payload)
-    : state;
+  type === ACTIONS.SET_GOAL ? parseInt(payload) : state;
 
 const skiDay = (state = null, { type, payload }) =>
-  type === ACTIONS.ADD_DAY
-    ? payload
-    : state;
+  type === ACTIONS.ADD_DAY ? payload : state;
 
-export { goal, skiDay };
+const error = (state = [], { type, payload }) => {
+  switch (type) {
+    case ACTIONS.ADD_ERROR:
+      return [...state, payload];
+    case ACTIONS.CLEAR_ERROR:
+      return state.filter((_, i) => i !== payload);
+    default:
+      return state;
+  }
+};
+
+export { goal, skiDay, error };
