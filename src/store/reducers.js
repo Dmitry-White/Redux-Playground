483f5c4412f6e211206d46d29678c41c84848e32
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-01-25 23:19:35 +0300

    Refactor reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,19 +1,13 @@
 import ACTIONS from '../constants';
 
-const goal = (state = 10, { type, payload }) => {
-  if (type === ACTIONS.SET_GOAL) {
-    return parseInt(payload);
-  } else {
-    return state;
-  }
-};
+const goal = (state = 10, { type, payload }) =>
+  type === ACTIONS.SET_GOAL
+    ? parseInt(payload)
+    : state;
 
-const skiDay = (state = null, { type, payload }) => {
-  if (type === ACTIONS.ADD_DAY) {
-    return payload;
-  } else {
-    return state;
-  }
-};
+const skiDay = (state = null, { type, payload }) =>
+  type === ACTIONS.ADD_DAY
+    ? payload
+    : state;
 
 export { goal, skiDay };
